cc6c1cca9e49f9db5ad5b0a524cd5c273d416200
Steven Huang <steven@stevenhuang.ca>
2014-10-01 01:15:53 -0700

    Added symlink script, tmux confs

---
+++ symlink.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dotfiles=".bashrc .vimrc .tmux.conf"
+dotfilesDIR=~/dotfiles
+for dotfile in $dotfiles; do
+	echo "Creating symlink for $dotfile"
+	echo -e "\tln -s $dotfilesDIR/$dotfile ~/$dotfile"
+	ln -s $dotfilesDIR/$dotfile ~/$dotfile
+done
+# ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
