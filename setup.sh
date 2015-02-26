a226a87bbf168952af29f1cb109b37e41a5c6120
Steven Huang <stevenhuang1995@gmail.com>
2015-02-25 23:12:37 -0500

    Renamed to setup.sh, add fonts on setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+sh fonts/install.sh
+echo
 dotfiles=".vim .bashrc .vimrc .tmux.conf"
 dotfilesDIR=~/dotfiles
 for dotfile in $dotfiles; do
 	echo "Creating symlink for $dotfile"
 	echo -e "\tln -s $dotfilesDIR/$dotfile ~/$dotfile"
 	ln -s $dotfilesDIR/$dotfile ~/$dotfile
+	echo
 done
 # ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
